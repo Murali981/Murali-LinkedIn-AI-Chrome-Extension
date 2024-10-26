5d19127ed91cb0fe737f79299ca7ca3dc516e74a
Murali981 <muraliju981@gmail.com>
2024-10-26 14:23:26 +0530

    Added a backupcode-v5 file to backup this version of the code

--- .wxt/types/paths.d.ts
+++ .wxt/types/paths.d.ts
@@ -12,6 +12,7 @@ declare module "wxt/browser" {
     | "/backupcode-v2.js"
     | "/backupcode-v3.js"
     | "/backupcode-v4.js"
+    | "/backupcode-v5.js"
     | "/content-scripts/content.js"
     | "/icon/128.png"
     | "/icon/16.png"
